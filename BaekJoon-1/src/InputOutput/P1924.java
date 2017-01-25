dd28fe41dd93a55a217a66e01fd7d5516110343a
solpark <cacadew13@gmail.com>
2017-01-25 12:50:18 +0900

    문제 설명 추가

--- BaekJoon-1/src/InputOutput/P1924.java
+++ BaekJoon-1/src/InputOutput/P1924.java
@@ -1,13 +1,13 @@
 package InputOutput;
 
 import java.util.Scanner;
-
+/*
+ * 오늘은 2007년 1월 1일 월요일이다.
+ * 그렇다면 2007년 x월 y일은 무슨 요일일까? 이를 알아내는 프로그램을 작성하시오.
+ * 첫째 줄에 x월 y일이 무슨 요일인지에 따라 SUN, MON, TUE, WED, THU, FRI, SAT중 하나를 출력한다.
+ * */
 public class P1924 {
 	public static void main(String[] args) {
-		
-		/*final int SUN = 0;
-		final int MON = 1;
-		final int TUE = 2;*/
 		
 		Scanner sc = new Scanner(System.in);
 		
